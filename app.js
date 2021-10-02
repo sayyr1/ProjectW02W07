d7768213a806bbb296d61e07322f085f699be732
sayyr1 <edwinsay03@gmail.com>
2021-10-02 15:12:33 -0500

    WEEK 3

--- app.js
+++ app.js
@@ -1,44 +1,25 @@
-const express = require('express')
+const path = require('path');
 
+const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
 
 const app = express();
-
-const path = require('path')
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
-let products = []
-
-app.get('/', (req, res, next) => {
-    res.render('index', { pageTitle: 'Add Products'})
-})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.get('/products', (req, res, next) => {
-    res.render('product', { pageTitle: 'Products', products: products })
-})
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.post('/add-products', (req, res, next) => {
-    products.push({ name: req.body.product, number: req.body.number, category: req.body.category  })
-    res.redirect('/products')
-    console.log(products)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-})
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.post('/remove-products', (req, res, next) => {
+app.use(errorController.get404);
 
-    const removeProd = req.body.product;
+app.listen(process.env.PORT || 3000);
 
-    console.log(products)
-
-    products = products.filter(product => product.name !== removeProd)
-
-    res.redirect('/products')
-})
-
-app.listen(process.env.PORT||3000)
